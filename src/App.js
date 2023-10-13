2f51edd3f052443b7799ae3aef84e75a5d9a60d6
adish-ct <adishathu525@gmail.com>
2023-10-13 10:29:44 +0530

    home page added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
+import HomePage from "./pages/HomePage";
+import { Route, BrowseRouter as Router, Route } from "react-router-dom";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
 }
 
 export default App;
